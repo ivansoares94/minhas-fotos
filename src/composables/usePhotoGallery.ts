5aeaf64e5d0e03ba4719e9755397eabd219afdb9
ivansoares1994 <ivansoares1994@gmail.com>
2021-10-27 21:17:18 -0300

    First commit

---
+++ src/composables/usePhotoGallery.ts
@@ -0,0 +1,72 @@
+import { ref, onMounted, watch } from 'vue';
+import { Capacitor } from '@capacitor/core';
+import {
+  Camera,
+  CameraSource,
+  CameraResultType,
+  Photo,
+} from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Storage } from '@capacitor/storage';
+
+export function usePhotoGallery() {
+  const photos = ref<UserPhoto[]>([]);
+  const PHOTO_STORAGE = 'photos';
+  
+  const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
+      const file = await Filesystem.readFile({
+        path: photo.path!,
+      });
+      const base64Data = file.data;
+
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+
+    return {
+      filepath: savedFile.uri,
+      webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+    };
+  };
+
+  const loadSaved = async () => {
+    const photoList = await Storage.get({ key: PHOTO_STORAGE });
+    const photosInStorage = photoList.value ? JSON.parse(photoList.value) : [];
+    photos.value = photosInStorage;
+  };
+
+  const takePhoto = async () => {
+    const cameraPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100,
+    });
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFileImage = await savePicture(cameraPhoto, fileName);
+
+    photos.value = [savedFileImage, ...photos.value];
+  };
+
+  const cachePhotos = () => {
+    Storage.set({
+      key: PHOTO_STORAGE,
+      value: JSON.stringify(photos.value),
+    });
+  };
+
+  onMounted(loadSaved);
+
+  watch(photos, cachePhotos);
+
+  return {
+    photos,
+    takePhoto
+  };
+}
+
+export interface UserPhoto {
+  filepath: string;
+  webviewPath?: string;
+}
---
+++ src/views/Home.vue
@@ -0,0 +1,77 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Galeria de foto</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Galeria de foto</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <ion-grid>
+        <ion-row>
+          <ion-col size="3" :key="photo" v-for="photo in photos">
+            <ion-img
+              :src="photo.webviewPath"
+            ></ion-img>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+
+      <ion-fab vertical="bottom" horizontal="center">
+        <ion-fab-button @click="takePhoto()">
+          <ion-icon :icon="camera"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonPage,
+         IonHeader,
+         IonFab,
+         IonFabButton,
+         IonIcon,
+         IonToolbar,
+         IonTitle,
+         IonContent,
+         IonGrid,
+         IonRow,
+         IonCol,
+         IonImg } from '@ionic/vue';
+import { defineComponent } from 'vue';
+import { camera, trash, close } from 'ionicons/icons';
+import { usePhotoGallery } from '@/composables/usePhotoGallery';
+
+export default defineComponent({
+  name: 'Home',
+  components: { IonPage,
+                IonHeader,
+                IonFab,
+                IonFabButton,
+                IonIcon,
+                IonToolbar,
+                IonTitle,
+                IonContent,
+                IonGrid,
+                IonRow,
+                IonCol,
+                IonImg },
+  setup() {
+    const { photos, takePhoto,
+      } = usePhotoGallery();
+
+    return {
+      photos,
+      takePhoto,
+      camera,
+      trash,
+      close,
+    };
+  },
+});
+</script>
